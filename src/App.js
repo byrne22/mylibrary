f01df8b25ce832bbf608019a39864ffc83dcba18
byrne22 <byron.odhiambo@student.moringaschool.com>
2022-07-31 02:37:11 +0300

    route components to ap.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import BookList from "./components/BookList";
+import BookDetails from "./components/BookDetails";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Favorites from "./components/Favorites";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<BookList />} />
+        <Route path="/books/:id" element={<BookDetails />} />
+        <Route path="/favorites" element={<Favorites />} />
+      </Routes>
+      <Footer />
     </div>
   );
 }
