f44da76cba3f2a644802a9e15bb12a16aceab358
byrne22 <byron.odhiambo@student.moringaschool.com>
2022-07-31 03:05:59 +0300

    map books add favoriteschecker

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -24,4 +24,37 @@ const BookList = () => {
           })
           .catch((err) => console.log(err));
       }, []);
-      
+      return (
+        <div className="book-list">
+          {books.map((book) => (
+            <div key={book.id} className="book">
+              <div>
+                <h4>{book.title}</h4>
+              </div>
+              <div>
+                <img
+                  src={book.image_url}
+                  alt="#"
+                  onClick={() => navigate(`/books/${book.id}`)}
+                />
+              </div>
+              <div>
+                {favoritesChecker(book.id) ? (
+                  <button onClick={() => removeFromFavorites(book.id)}>
+                    Remove from Favorites
+                  </button>
+                ) : (
+                  <button onClick={() => addToFavorites(book)}>
+                    Add to Favorites
+                  </button>
+                )}
+              </div>
+            </div>
+          ))}
+        </div>
+      );
+    };
+    
+    export default BookList;
+    
+
