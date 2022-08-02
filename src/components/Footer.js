4fabc4edc56b3af8f1c0282687b11b936d20d33f
byrne22 <byron.odhiambo@student.moringaschool.com>
2022-08-02 19:04:35 +0300

    edit footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ import '../App';
 const Footer = () => {
   return (
     <div className="footer">
-      <p>My library Catalogue Designed by Byron &copy;2022</p>
+      <p>My library Catalogue Webapp   Designed by Byron &copy;2022</p>
     </div>
   );
 };
